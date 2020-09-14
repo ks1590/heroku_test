a77b20abf8ea37be897f6aeedbf2eca1f43ca926
kensuke suzuki <35k.0101@gmail.com>
2020-09-14 22:32:41 +0900

    変数を定義 ’ ’ ’変数を定義

---
+++ script.js
@@ -0,0 +1,8 @@
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    single_mealEl = document.getElementsById('single-meal');
+
+
