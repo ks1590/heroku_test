6e4a2285d3c8f189a48f95f07ff64105517735e9
kensuke suzuki <35k.0101@gmail.com>
2020-09-15 22:39:10 +0900

    APIからデータ取得、検索機能追加

--- script.js
+++ script.js
@@ -3,6 +3,45 @@ const search = document.getElementById('search'),
     random = document.getElementById('random'),
     mealsEl = document.getElementById('meals'),
     resultHeading = document.getElementById('result-heading'),
-    single_mealEl = document.getElementsById('single-meal');
+    single_mealEl = document.getElementById('single-meal');
 
+//Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
 
+    // Clear single meal
+    single_mealEl.innerHTML = ''
+
+    // Get searcj term
+    const term = search.value;
+
+    // Check for empty
+    if (term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                resultHeading.innerHTML = `<h2>Search resilts for '${term}':<h2>`
+
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
+                } else {
+                    mealsEl.innerHTML = data.meals.map(meal => `
+                    <div class = "meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data-mealID="${meal.idMeal}">
+                    <h3>${meal.strMeal}</h3>
+                </div>
+                    </div>
+                    `)
+                        .join('');
+                }
+            });
+        search.value = '';
+    } else {
+        alert('Please enter a serch term');
+    }
+}
+
+//Event listeners
+submit.addEventListener('submit', searchMeal);
